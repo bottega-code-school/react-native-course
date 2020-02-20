0c8fef024c33e9a929bf867d268ae6ede4fb2d9c
jordanhudgens <jordan@devcamp.com>
2020-02-20 10:41:01 -0700

    A_03_react_native_expo_project_file_walkthrough

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Text changed</Text>
     </View>
   );
 }
@@ -12,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
