8454b25b52d46487f20819b7388c7d15946a8892
jordanhudgens <jordan@devcamp.com>
2020-02-24 09:16:20 -0500

    A_07_navigating_between_screens

--- App.tsx
+++ App.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 
 import router from "./utils/router";
 import { createAppContainer } from "react-navigation";
 const AppContainer = createAppContainer(router);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppContainer />
-    </View>
-  );
+  return <AppContainer />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default () => {
+export default props => {
   return (
     <View>
       <Text>Feed screen</Text>
+
+      <TouchableOpacity onPress={() => props.navigation.navigate("Search")}>
+        <Text>Search</Text>
+      </TouchableOpacity>
     </View>
   );
 };
