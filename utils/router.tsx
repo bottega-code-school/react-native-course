41aff459c1b6a0379528c3ea518406027819e841
jordanhudgens <jordan@devcamp.com>
2020-02-26 08:04:50 -0500

    A_09_creating_base_set_of_screens

--- utils/router.tsx
+++ utils/router.tsx
@@ -4,11 +4,15 @@ import { createStackNavigator } from "react-navigation-stack";
 
 import FeedScreen from "../screens/FeedScreen";
 import SearchScreen from "../screens/SearchScreen";
+import AccountScreen from "../screens/AccountScreen";
+import PostFormScreen from "../screens/PostFormScreen";
 
 const AppStack = createStackNavigator(
   {
     Feed: FeedScreen,
-    Search: SearchScreen
+    Search: SearchScreen,
+    Account: AccountScreen,
+    PostForm: PostFormScreen
   },
   {
     initialRouteName: "Feed"
