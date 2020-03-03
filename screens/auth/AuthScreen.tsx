a2c285e1faa2e43d1046bb526afaa1070dc57dcc
jordanhudgens <jordan@devcamp.com>
2020-03-03 10:00:24 -0700

    C_02_dynamically_rendering_content_react_hooks

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 export default () => {
+  const [formToShow, setFormToShow] = useState("LOGIN");
+
+  const screenTypeText = () => {
+    if (formToShow === "LOGIN") {
+      return "Need an account? Register";
+    } else if (formToShow === "REGISTER") {
+      return "Already have an account? Login";
+    }
+  };
+
+  const handleAuthTypePress = () => {
+    if (formToShow === "LOGIN") {
+      setFormToShow("REGISTER");
+    } else if (formToShow === "REGISTER") {
+      setFormToShow("LOGIN");
+    }
+  };
+
+  const headerText = () => {
+    if (formToShow === "LOGIN") {
+      return "Login";
+    } else if (formToShow === "REGISTER") {
+      return "Register";
+    }
+  };
+
   return (
     <View style={{ marginTop: 100 }}>
-      <Text>Login</Text>
+      <Text>{headerText()}</Text>
 
-      <TouchableOpacity>
-        <Text>Already have an account? Register</Text>
+      <TouchableOpacity onPress={handleAuthTypePress}>
+        <Text>{screenTypeText()}</Text>
       </TouchableOpacity>
     </View>
   );
