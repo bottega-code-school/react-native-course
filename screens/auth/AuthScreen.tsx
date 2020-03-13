9278c6fa3d6ae53f737895f11315da126a87437d
jordanhudgens <jordan@devcamp.com>
2020-03-13 09:12:38 -0700

    C_09_implementing_jwt_authentication_react_native

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import axios from "axios";
 
 import textInputStyles from "../../styles/forms/textInputStyles";
 const { textFieldWrapper, textField } = textInputStyles;
 import authScreenStyles from "../../styles/stacks/auth/authScreenStyles";
+
+const apiEndpoint =
+  "https://reactnative.devcamp.space/memipedia/memipedia_user_token";
 
 export default () => {
   const [formToShow, setFormToShow] = useState("LOGIN");
@@ -34,6 +38,23 @@ export default () => {
     }
   };
 
+  const handleSubmit = () => {
+    const params = {
+      auth: {
+        email: email,
+        password: password
+      }
+    };
+    axios
+      .post(apiEndpoint, params)
+      .then(response => {
+        console.log("Response from handle submit", response.data);
+      })
+      .catch(error => {
+        console.log("error getting token", error);
+      });
+  };
+
   return (
     <View style={authScreenStyles.container}>
       <Text style={{ color: "white" }}>{headerText()}</Text>
@@ -61,6 +82,10 @@ export default () => {
 
       <TouchableOpacity onPress={handleAuthTypePress}>
         <Text style={{ color: "white" }}>{screenTypeText()}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleSubmit}>
+        <Text style={{ color: "white" }}>{headerText()}</Text>
       </TouchableOpacity>
     </View>
   );
