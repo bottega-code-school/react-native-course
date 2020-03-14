05d7e3f77173a59259ab922af4947b4aa5634467
jordanhudgens <jordan@devcamp.com>
2020-03-14 09:15:40 -0700

    C_11_creating_api_utility_helper_function_react_native

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, TextInput } from "react-native";
-import axios from "axios";
 
 import textInputStyles from "../../styles/forms/textInputStyles";
 const { textFieldWrapper, textField } = textInputStyles;
 import authScreenStyles from "../../styles/stacks/auth/authScreenStyles";
-
-const apiEndpoint =
-  "https://reactnative.devcamp.space/memipedia/memipedia_user_token";
+import API from "../../utils/api";
 
 export default () => {
   const [formToShow, setFormToShow] = useState("LOGIN");
@@ -45,8 +42,7 @@ export default () => {
         password: password
       }
     };
-    axios
-      .post(apiEndpoint, params)
+    API.post("memipedia_user_token", params)
       .then(response => {
         console.log("Response from handle submit", response.data);
       })
