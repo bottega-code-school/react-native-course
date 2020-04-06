1bc06d2fe93d6be0abf22cf9f15af3afa8692cd8
jordanhudgens <jordan@devcamp.com>
2020-04-06 11:29:09 -0700

    C_27_auto_populating_current_user_after_login

--- providers/CurrentUserProvider.tsx
+++ providers/CurrentUserProvider.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
 import CurrentUserContext from "../contexts/CurrentUserContext";
+
+import api from "../utils/api";
 
 interface ICurrentUserProviderProps {
   children: any;
@@ -8,12 +11,36 @@ interface ICurrentUserProviderProps {
 export default (props: ICurrentUserProviderProps) => {
   const [currentUser, setCurrentUser] = useState({
     id: 123,
-    email: "hey@you.com"
+    email: "hey@you.com",
   });
+
+  const getUser = async () => {
+    const token = await SecureStore.getItemAsync("memipedia_secure_token");
+
+    api
+      .get("logged_in", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log("response from getUser", response.data);
+
+        if (response.data.memipedia_user) {
+          setCurrentUser(response.data.memipedia_user);
+        } else {
+          setCurrentUser(null);
+        }
+      })
+      .catch((error) => {
+        setCurrentUser(null);
+      });
+  };
 
   const stateValues = {
     currentUser,
-    setCurrentUser
+    setCurrentUser,
+    getUser,
   };
 
   return (
--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -28,7 +28,7 @@ export default (props: IAuthScreenProps) => {
   const [password, setPassword] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const { currentUser } = useContext(CurrentUserContext);
+  const { currentUser, getUser } = useContext(CurrentUserContext);
 
   const screenTypeText = () => {
     if (formToShow === "LOGIN") {
@@ -68,6 +68,7 @@ export default (props: IAuthScreenProps) => {
             "memipedia_secure_token",
             response.data.jwt
           );
+          getUser();
           props.navigation.navigate("Feed");
         } else {
           alert(
@@ -96,7 +97,7 @@ export default (props: IAuthScreenProps) => {
       .then((response) => {
         console.log("Res for creating user", response.data);
         if (response.data.memipedia_user) {
-          props.navigation.navigate("Feed");
+          handleLogin();
         } else {
           alert(
             `Error creating account: ${formatErrors(response.data.errors)}`
