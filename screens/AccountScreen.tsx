1d62237a3c5ada7e2dbdd97e2c313c506bb40e51
jordanhudgens <jordan@devcamp.com>
2020-04-06 10:55:20 -0700

    C_26_logging_users_out

--- screens/AccountScreen.tsx
+++ screens/AccountScreen.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text } from "react-native";
+import * as SecureStore from "expo-secure-store";
 
-export default () => {
+import CurrentUserContext from "../contexts/CurrentUserContext";
+import Button from "../components/helpers/Button";
+
+interface IAccountScreenProps {
+  navigation: {
+    navigate: (arg: string) => void;
+  };
+}
+export default (props: IAccountScreenProps) => {
+  const { setCurrentUser } = useContext(CurrentUserContext);
+
+  const handleSignOut = async () => {
+    await SecureStore.deleteItemAsync("memipedia_secure_token");
+    setCurrentUser(null);
+    props.navigation.navigate("Auth");
+  };
+
   return (
     <View>
       <Text>Account screen</Text>
+
+      <View style={{ marginTop: 20 }}>
+        <Button onPress={handleSignOut} text="Sign Out" />
+      </View>
     </View>
   );
 };
