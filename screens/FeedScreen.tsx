32325256c155a30da44adcae955346ed212d2598
jordanhudgens <jordan@devcamp.com>
2020-04-09 11:29:56 -0700

    D_02_api_call_for_posts

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
-
-import CurrentUserContext from "../contexts/CurrentUserContext";
+import * as SecureStore from "expo-secure-store";
 
 import Container from "../components/layouts/Container";
+import api from "../utils/api";
 
 interface IFeedScreenProps {
   navigation: {
@@ -11,12 +11,35 @@ interface IFeedScreenProps {
   };
 }
 export default (props: IFeedScreenProps) => {
-  const { currentUser } = useContext(CurrentUserContext);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    const token = await SecureStore.getItemAsync("memipedia_secure_token");
+
+    api
+      .get("memipedia_posts", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log("res from posts", response.data);
+        setPosts(response.data.memipedia_posts);
+      })
+      .catch((error) => {
+        console.log("error from posts", error);
+      });
+  };
+
   return (
     <Container navigate={props.navigation.navigate}>
       <Text>Feed screen</Text>
       <View style={{ marginTop: 20 }}>
-        <Text>{JSON.stringify(currentUser)}</Text>
+        <Text>{JSON.stringify(posts)}</Text>
       </View>
     </Container>
   );
