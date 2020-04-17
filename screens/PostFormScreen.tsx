10ffade2045c0b5dbc7539af68493cac2a5efe9c
jordanhudgens <jordan@devcamp.com>
2020-04-17 10:18:27 -0700

    E_04_building_initial_post_form

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -1,16 +1,34 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput } from "react-native";
 
 import PostImagePicker from "../components/posts/PostImagePicker";
+import Button from "../components/helpers/Button";
 
 export default () => {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <View style={{ height: "100%" }}>
-      <Text>Post form screen</Text>
+      <TextInput
+        placeholder="Name"
+        value={name}
+        onChangeText={(val) => setName(val)}
+      />
+
+      <TextInput
+        placeholder="Add meme explanation here"
+        value={content}
+        onChangeText={(val) => setContent(val)}
+        style={{ borderWidth: 2, borderColor: "black" }}
+        multiline
+      />
 
       <View style={{ marginTop: 40, height: 100 }}>
         <PostImagePicker />
       </View>
+
+      <Button text="Submit" onPress={() => console.log("Submitting...")} />
     </View>
   );
 };
