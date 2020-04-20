0564239f5b85adc79b08f1314918ceb0ff8093f3
jordanhudgens <jordan@devcamp.com>
2020-04-20 10:30:01 -0700

    E_07_wrapping_image_data_in_form_object

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -9,6 +9,25 @@ export default () => {
   const [content, setContent] = useState("");
   const [postImage, setPostImage] = useState(null);
 
+  const buildForm = () => {
+    let formData = new FormData();
+
+    formData.append("post[name]", name);
+    formData.append("post[content]", content);
+
+    const uriParts = postImage.split(".");
+    const fileType = uriParts[uriParts.length - 1];
+
+    formData.append("post[post_image]", {
+      // @ts-ignore
+      uri: postImage,
+      name: `photo.${fileType}`,
+      type: `image/${fileType}`,
+    });
+
+    return formData;
+  };
+
   return (
     <View style={{ height: "100%" }}>
       <TextInput
