44fb0b4500a2afa5acf6f91f4addd244ebc3e8c5
jordanhudgens <jordan@devcamp.com>
2020-04-23 14:13:37 -0700

    F_01_creating_initial_search_bar

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import Container from "../components/layouts/Container";
 
 interface ISearchScreenProps {
@@ -8,9 +8,32 @@ interface ISearchScreenProps {
   };
 }
 export default (props: ISearchScreenProps) => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    console.log("Searching for", query);
+  };
+
+  const searchBar = (
+    <View>
+      <TextInput
+        value={query}
+        onChangeText={(val) => setQuery(val)}
+        placeholderTextColor="white"
+        placeholder="Search for a meme"
+        onSubmitEditing={handleSearch}
+      />
+
+      <TouchableOpacity style={{ marginTop: 20 }} onPress={handleSearch}>
+        <Text style={{ color: "white" }}>Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <Container navigate={props.navigation.navigate}>
       <Text>SearchScreen</Text>
+      {searchBar}
     </Container>
   );
 };
